c4e255f7c1d1693bc10e7a12cd23cb43dbd37a10
Sterling Engle <sengle@Zeus.localdomain>
2019-07-21 09:10:15 -0700

    coded make_sudoku_board.c and print_sudoku.c they need to be tested; added a Makefile

--- ex00/make_sudoku_board.c
+++ ex00/make_sudoku_board.c
@@ -6,19 +6,48 @@
 /*   By: sengle <sengle@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/20 09:11:38 by sengle            #+#    #+#             */
-/*   Updated: 2019/07/20 10:27:32 by sengle           ###   ########.fr       */
+/*   Updated: 2019/07/21 09:07:51 by sengle           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
 #include <stdlib.h>
+#include "sudoku.h"
 
 /*
 **	make_sudoku_board(argc, argv) converts argv[1..9] to an int sudoku array.
 **		It returns NULL if argc != 10 or there is invalid input.
+**
+**	local variables:
+**
+**	board	int ** pointer to a pointer (double pointer)
+**			First, we allocate an array of pointers to each row.
+**			Then, we allocate memory to hold each column for that board[row]
+**
+**	row		board[row] and its corresponding argv[row + 1] (argv[0] is program)
+**	col		board[row][col]
 */
-int		**make_sudoku_board(int argc, char **argv)
+
+int	**make_sudoku_board(int argc, char **argv)
 {
+	int	**board;
+	int	row;
+	int	col;
+
 	if (argc != 10)
 		return (NULL);
+	row = 0;
+	board = (int **)malloc(ROWS * sizeof(int *));
+	while (row < ROWS)
+		board[row] = (int *)malloc(COLS * sizeof(int));
+	row = 0;
+	col = 0;
+	while (row < ROWS)
+		while (col < COLS) {
+			if (argv[row + 1][col] == '.')
+				board[row][col] = 0;
+			else if ('1' <= argv[row + 1][col] && argv[row + 1][col] <= '9')
+				board[row][col] = argv[row + 1][col] - '0';
+			else
+				return (NULL);
+		}
 }
---
+++ ex00/sudoku.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sudoku.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sengle <sengle@student.42.us.org>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/21 02:38:39 by sengle            #+#    #+#             */
+/*   Updated: 2019/07/21 02:41:53 by sengle           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SUDOKU_H
+# define SUDOKU_H
+# define ROWS 9
+# define COLS 9
+# include <unistd.h>
+# define TRUE 1
+# define FALSE 0
+# define SUCCESS 0
+
+typedef unsigned char	t_bool;
+#endif
